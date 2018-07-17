713fe72fd4868a828dfcc54abe49ade9dcf624f4
maiconvos <maiconvos@gmail.com>
2018-07-17 01:52:41 -0300

    Add migrates and models to links aggregator.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_035207) do
+ActiveRecord::Schema.define(version: 2018_07_17_044546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 2018_07_11_035207) do
 
   create_table "hashtags", force: :cascade do |t|
     t.string "name"
+    t.integer "company_id"
+  end
+
+  create_table "link_hashtags", force: :cascade do |t|
+    t.integer "link_id"
+    t.integer "hashtag_id"
+  end
+
+  create_table "links", force: :cascade do |t|
+    t.string "url"
     t.integer "company_id"
   end
 
